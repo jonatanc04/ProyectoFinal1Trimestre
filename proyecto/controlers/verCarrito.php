9ab7ff91131fc522765d92c343b550fe68059553
Jonatan Córcoles <135428009+jonatanc04@users.noreply.github.com>
2023-11-09 19:26:42 +0100

    Add files via upload

---
+++ proyecto/controlers/verCarrito.php
@@ -0,0 +1,15 @@
+<?php
+
+include "../config/autocarga.php";
+include "../view/header.php";
+$base = new Base();
+
+echo "<div id='body' class='row'>";
+echo "<div id='prodConten'>";
+echo "<script src='../js/gestionCarrito.js'></script>";
+echo "</div>";
+echo "</div>";
+
+include "../view/footer.html";
+
+?>
